5eef06cc5dfcd9a77af179549f10651044dec410
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 23:05:23 +0800

    Update arp.py

--- arp/source/arp.py
+++ arp/source/arp.py
@@ -1,6 +1,8 @@
 import binascii
 from struct import pack, unpack
 from socket import socket, AF_PACKET, SOCK_RAW, htons
+
+# create a raw socket
 s = socket(AF_PACKET, SOCK_RAW, htons(0x0806)) # ARP type number(0x0800 for IP, 0x0003 for all ethernet frame)
 s.bind(('eth0', 0))
 
@@ -24,7 +26,6 @@ frame = [
     pack('!6B', *[0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), # Target MAC address
     pack('!4B', *[192, 168, 1, 20])  # Target IP address
 ]
-
 s.send(b''.join(frame))
 
 # receive the frame & unpack it
@@ -32,6 +33,8 @@ s.send(b''.join(frame))
 frame = s.recvfrom(2048)
 ethernet_frame = unpack("!6s6s2s", frame[0][0:14]) # extract ethernet frame
 arp_frame = unpack("2s2s1s1s2s6s4s6s4s", frame[0][14:42]) # extract arp frame
+
+# extract target host MAC address in the arp frame
 target_mac = binascii.hexlify(arp_frame[5]).decode().upper()
 target_mac = ':'.join(target_mac[x:y] for x, y in zip(range(0, 10 + 1, 2), range(2, 12 + 1, 2)))
 print(f'Target Host MAC address: {target_mac}')
