04696bae89074eeb589fa1aa4187755c0bf68098
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 23:28:08 +0800

    Update client.py

--- remote-keyboard/source/client.py
+++ remote-keyboard/source/client.py
@@ -10,6 +10,7 @@ def keydownEventHanlder(key):
 try:
     server_addr = ('server_ip', 6666)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
     keys = ['esc', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', '`',
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '=', 'backspace', 'tab',
             'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'caps lock',
