de59fc397357ada1bd3b4e57f0447977d4cd6e5a
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 16:47:23 +0800

    upload UDP-socket

---
+++ UDP-socket/source/client.py
@@ -0,0 +1,17 @@
+import socket
+
+server_addr = ('localhost', 6666)
+
+# create an UDP socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send a message to the server
+msg = 'msg'
+udp_socket.sendto(msg.encode(), server_addr)
+print(f"Message: '{msg}' has been sent")
+
+# receive a new message from the server
+new_msg, return_addr = udp_socket.recvfrom(1024)
+print(f"New message: '{new_msg.decode()}' has received [from {return_addr}]")
+
+udp_socket.close()
---
+++ UDP-socket/source/server.py
@@ -0,0 +1,21 @@
+import socket
+
+server_addr = ('0.0.0.0', 6666)
+
+# create an UDP socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(server_addr)
+print('Ready to receive UDP packets')
+
+while True:
+    # receive a message from the client
+    msg, client_addr = udp_socket.recvfrom(1024)
+    print(f"Received message: '{msg.decode()}' from {client_addr}")
+
+    # send a new message back to the client
+    new_msg = msg.decode().upper()
+    udp_socket.sendto(new_msg.encode(), client_addr)
+    print(f"Sent new message: '{new_msg}' back to {client_addr}")
+# end while
+
+udp_socket.close()
