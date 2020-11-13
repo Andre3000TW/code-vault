77af3f84ac7ab3b1ca8fba686f6230649e0d6e6d
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 15:45:52 +0800

    upload mouse-logger

---
+++ mouse-logger/README.md
@@ -0,0 +1,13 @@
+# Mouse Logger
+A program that can be used to log mouse events(including move/wheel/button events).
+
+# Usage
+1. Open source/mouse-logger.py (change extension to .pyw to run it in the background)
+2. Mouse log file will be generated under the same directory of mouse-logger.py
+
+# Test Environment
++ Windows 10
++ Python 3.9.0
+
+# Package Requirements
++ mouse==0.7.1
---
+++ mouse-logger/source/mouse-logger.py
@@ -0,0 +1,18 @@
+import time
+import mouse
+
+def logger(event):
+    with open(r'mouselog.txt', 'a') as mouselog:
+        msg = ''
+        if type(event).__name__ == 'MoveEvent': msg = f'move:{event.x},{event.y}'
+        elif type(event).__name__ == 'WheelEvent': msg = f'wheel:{event.delta}'
+        elif type(event).__name__ == 'ButtonEvent': msg = f'button {event.event_type}:{event.button}'
+        else: msg = 'error:unknown event'
+        mouselog.write(msg + '\n')
+        print(msg)
+    # end with-as
+# end logger()
+
+mouse.hook(logger)
+
+while True: time.sleep(1)
