76ee7dc91089f73caf8afb44787168a4901a0a52
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 23:31:25 +0800

    Update server.py

--- remote-mouse/source/server.py
+++ remote-mouse/source/server.py
@@ -5,20 +5,18 @@ from bs4 import BeautifulSoup
 import mouse
 from datetime import datetime
 
-# get ip addr
+# get IP address
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 page = requests.get('https://whatismyipaddress.com', headers=headers)
 page.encoding = 'utf8'
 soup = BeautifulSoup(page.text, 'lxml')
 ip_addr = soup.find('a', text='Show Complete IP Details').get('href').split('/')[-1]
-# set host ip & port num
-host = '0.0.0.0'
-port = 6666
-# create a udp socketad
+
+# create an UDP socket
+server_addr = ('0.0.0.0', 6666)
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind((host, port))
-# ready to receive commands
-print(f'Ready to receive commands on port: {port} with public IP addr: {ip_addr}')
+udp_socket.bind(server_addr)
+print(f'Ready to receive commands on port: {server_addr[1]} with public IP addr: {ip_addr}')
 
 while True:
     msg, client_addr = udp_socket.recvfrom(1024)
