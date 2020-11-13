103f3f8f9f4de9c1659747ee15829f0ebd5242cd
Andre3000TW <andre3000tw@gmail.com>
2020-11-13 23:29:59 +0800

    Update server.py

--- remote-keyboard/source/server.py
+++ remote-keyboard/source/server.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import keyboard
 from datetime import datetime
 
-# get ip addr
+# get IP address
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 page = requests.get('https://whatismyipaddress.com', headers=headers)
 page.encoding = 'utf8'
